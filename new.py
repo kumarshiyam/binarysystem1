6b610715daea09474bc7343c80d6e2d770172558
shiyam <shiyamkrishnaa@gmail.com>
2023-07-23 23:56:17 -0700

    new file added now

---
+++ new.py
@@ -0,0 +1,12 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def binary_search(n, item):
+...     left, right= 0, len(n)
+...     while right > left:
+...         middle = (left + right) // 2
+...         if nums[middle] > item:
+...             right = middle
+...         elif nums[middle] < item :
+...             left = middle + 1
+...         else:
+...             return middle
