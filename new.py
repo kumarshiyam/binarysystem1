bcd725b7ef1c4bf0b38adbe886c231b2d26109be
shiyam <shiyamkrishnaa@gmail.com>
2023-07-24 23:50:49 -0700

    modified

--- new.py
+++ new.py
@@ -1,5 +1,6 @@
 Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.x
+
 >>> def binary_search(n, item):
 ...     left, right= 0, len(n)
 ...     while right > left:
